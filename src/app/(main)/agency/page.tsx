970c84b5c03f98d8ee40e6f4efbf1bfb2e089916
unknown <nikcevici13@gmail.com>
2024-04-08 00:26:53 +0200

    queries.ts

--- src/app/(main)/agency/page.tsx
+++ src/app/(main)/agency/page.tsx
@@ -1,7 +1,18 @@
+import { getAuthUserDetails, verifyAndAcceptInvitation } from "@/lib/queries";
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 import React from "react";
 
-function Page() {
+const Page = async () => {
+  const authUser = await currentUser();
+  if (!authUser) return redirect("/sign-in");
+  const agencyId = await verifyAndAcceptInvitation();
+  console.log(agencyId);
+
+  //get users details
+  const user = await getAuthUserDetails();
+
   return <div>Agency</div>;
-}
+};
 
 export default Page;
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,20 +1,23 @@
 import { authMiddleware } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 
+// This example protects all routes including api/trpc routes
+// Please edit this to allow other routes to be public as needed.
+// See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
   publicRoutes: ["/site", "/api/uploadthing"],
   async beforeAuth(auth, req) {},
   async afterAuth(auth, req) {
+    //rewrite for domains
     const url = req.nextUrl;
     const searchParams = url.searchParams.toString();
     let hostname = req.headers;
 
     const pathWithSearchParams = `${url.pathname}${
-      searchParams.length > 0 ? `${searchParams}` : ""
+      searchParams.length > 0 ? `?${searchParams}` : ""
     }`;
 
     //if subdomain exists
-
     const customSubDomain = hostname
       .get("host")
       ?.split(`${process.env.NEXT_PUBLIC_DOMAIN}`)
@@ -34,14 +37,14 @@ export default authMiddleware({
       url.pathname === "/" ||
       (url.pathname === "/site" && url.host === process.env.NEXT_PUBLIC_DOMAIN)
     ) {
-      return NextResponse.rewrite(new URL(`/site`, req.url));
+      return NextResponse.rewrite(new URL("/site", req.url));
     }
 
     if (
       url.pathname.startsWith("/agency") ||
       url.pathname.startsWith("/subaccount")
     ) {
-      return NextResponse.rewrite(new URL(`/${pathWithSearchParams}`, req.url));
+      return NextResponse.rewrite(new URL(`${pathWithSearchParams}`, req.url));
     }
   },
 });
