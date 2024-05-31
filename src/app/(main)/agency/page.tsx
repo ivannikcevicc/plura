de93b19f236ef6ad0c692d74e04d1633a547c0b0
unknown <nikcevici13@gmail.com>
2024-05-31 20:23:44 +0200

    adadad

--- src/app/(main)/agency/page.tsx
+++ src/app/(main)/agency/page.tsx
@@ -1,44 +1,46 @@
+import AgencyDetails from "@/components/forms/AgencyDetails";
+import { getAuthUserDetails, verifyAndAcceptInvitation } from "@/lib/queries";
 import { currentUser } from "@clerk/nextjs";
+import { Plan } from "@prisma/client";
 import { redirect } from "next/navigation";
 import React from "react";
-import { getAuthUserDetails, verifyAndAcceptInvitation } from "@/lib/queries";
-import { Plan } from "@prisma/client";
-import AgencyDetails from "@/components/forms/AgencyDetails";
 
-const Agency = async ({
-  params,
+const Page = async ({
+  searchParams,
 }: {
-  params: { plan: Plan; code: string; state: string };
+  searchParams: { plan: Plan; state: string; code: string };
 }) => {
   const agencyId = await verifyAndAcceptInvitation();
+  console.log(agencyId);
+
+  //get the users details
   const user = await getAuthUserDetails();
-
   if (agencyId) {
-    if (user?.role === "SUBACCOUNT_GUEST" || user?.role === "SUBACCOUNT_USER")
-      redirect("/subaccount");
-    else if (user?.role === "AGENCY_OWNER" || user?.role === "AGENCY_ADMIN") {
-      if (params.plan) {
-        return redirect(`/agency/${agencyId}/billing?plan=${params.plan}`);
-      }
-
-      if (params.state) {
-        const statePath = params.state.split("__")[0];
-        const stateAgencyId = params.state.split("__")[1];
-
-        if (!stateAgencyId) return <div>Not auth</div>;
+    if (user?.role === "SUBACCOUNT_GUEST" || user?.role === "SUBACCOUNT_USER") {
+      return redirect("/subaccount");
+    } else if (user?.role === "AGENCY_OWNER" || user?.role === "AGENCY_ADMIN") {
+      if (searchParams.plan) {
         return redirect(
-          `/agency/${stateAgencyId}/${statePath}?code=${params.code}`
+          `/agency/${agencyId}/billing?plan=${searchParams.plan}`
         );
       }
-    } else return redirect(`/agency/${agencyId}`);
-  } else return <div>Not auth</div>;
-
+      if (searchParams.state) {
+        const statePath = searchParams.state.split("___")[0];
+        const stateAgencyId = searchParams.state.split("___")[1];
+        if (!stateAgencyId) return <div>Not authorized</div>;
+        return redirect(
+          `/agency/${stateAgencyId}/${statePath}?code=${searchParams.code}`
+        );
+      } else return redirect(`/agency/${agencyId}`);
+    } else {
+      return <div>Not authorized</div>;
+    }
+  }
   const authUser = await currentUser();
-
   return (
     <div className="flex justify-center items-center mt-4">
-      <div className="max-w-[85-px] border-[1px] p-4 rounded-xl">
-        <h1 className="text-4xl ">Create an Agency</h1>
+      <div className="max-w-[850px] border-[1px] p-4 rounded-xl">
+        <h1 className="text-4xl"> Create An Agency</h1>
         <AgencyDetails
           data={{ companyEmail: authUser?.emailAddresses[0].emailAddress }}
         />
@@ -47,4 +49,4 @@ const Agency = async ({
   );
 };
 
-export default Agency;
+export default Page;
