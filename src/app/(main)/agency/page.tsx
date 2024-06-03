5de92f8bf0741adc5c013c1bef678f8bd9684a2f
unknown <nikcevici13@gmail.com>
2024-06-03 17:57:30 +0200

    adadad

--- src/app/(main)/agency/page.tsx
+++ src/app/(main)/agency/page.tsx
@@ -3,7 +3,6 @@ import { getAuthUserDetails, verifyAndAcceptInvitation } from "@/lib/queries";
 import { currentUser } from "@clerk/nextjs";
 import { Plan } from "@prisma/client";
 import { redirect } from "next/navigation";
-import React from "react";
 
 const Page = async ({
   searchParams,
