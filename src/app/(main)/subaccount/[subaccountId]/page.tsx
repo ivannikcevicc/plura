55c45e0f1358f51d4c2db78c1f47d1aa1b8cf879
unknown <nikcevici13@gmail.com>
2024-06-08 23:28:42 +0200

    ff

---
+++ src/app/(main)/subaccount/[subaccountId]/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SubaccountIdPage = () => {
+  return <div>SubaccountIdPage</div>;
+};
+
+export default SubaccountIdPage;
---
+++ src/app/(main)/subaccount/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SubaccountPage = () => {
+  return <div>SubaccountPage</div>;
+};
+
+export default SubaccountPage;
